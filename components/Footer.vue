c945067b06a42f462381fbe6a6e8e702beeb3817
tm0nt <tassio.leite14@gmail.com>
2024-03-29 14:24:16 -0300

    first commit

---
+++ components/Footer.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+const links = [{
+  label: 'Resources',
+  children: [{
+    label: 'Help center'
+  }, {
+    label: 'Docs'
+  }, {
+    label: 'Roadmap'
+  }, {
+    label: 'Changelog'
+  }]
+}, {
+  label: 'Features',
+  children: [{
+    label: 'Affiliates'
+  }, {
+    label: 'Portal'
+  }, {
+    label: 'Jobs'
+  }, {
+    label: 'Sponsors'
+  }]
+}, {
+  label: 'Company',
+  children: [{
+    label: 'About'
+  }, {
+    label: 'Pricing'
+  }, {
+    label: 'Careers'
+  }, {
+    label: 'Blog'
+  }]
+}]
+
+const toast = useToast()
+
+const loading = ref(false)
+
+</script>
+
+<template>
+  <UFooter>
+    <template #left>
+      <p class="text-gray-500 dark:text-gray-400 text-sm">
+        Copyright © {{ new Date().getFullYear() }}. Todos os direitos reservados.
+      </p>
+    </template>
+
+    <template #right>
+      <UColorModeButton size="sm" />
+
+      <UButton to="https://github.com/nuxt-ui-pro/landing" target="_blank" icon="i-simple-icons-github" aria-label="GitHub" color="gray" variant="ghost" />
+    </template>
+  </UFooter>
+</template>
---
+++ components/Placeholder.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="relative">
+        <img class="object-cover rounded-xl" src="https://assets-global.website-files.com/643b269c094e51b9d34a4a21/6479352fa9abff97d715bdc3_demo_img.webp"/>
+  </div>
+</template>
+
+<style scoped>
+.border-gradient:after {
+  --angle: 0deg;
+  --border-color: rgb(var(--color-gray-300));
+  --highlight-color: rgb(var(--color-primary-500));
+
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: -1px;
+  opacity: 0.5;
+  border-radius: 0.8rem;
+  z-index: -1;
+  background: var(--border-color);
+
+  @supports (background: paint(houdini)) {
+    background: linear-gradient(var(--angle), var(--border-color), var(--border-color), var(--border-color), var(--border-color), var(--highlight-color));
+    animation: 10s rotate linear infinite;
+  }
+}
+
+.dark .border-gradient:after {
+  --border-color: rgb(var(--color-gray-700));
+  --highlight-color: rgb(var(--color-gray-400));
+}
+
+@keyframes rotate {
+  to {
+    --angle: 360deg;
+  }
+}
+
+@property --angle {
+  syntax: '<angle>';
+  initial-value: 0deg;
+  inherits: false;
+}
+</style>
